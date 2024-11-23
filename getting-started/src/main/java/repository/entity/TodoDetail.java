9c4d77594b12f0654f6732ce0b5029acfbdf4c85
dy <56531152+dy7ss@users.noreply.github.com>
2024-11-23 22:20:50 +0900

    fix: TodoDetailの不要なフィールドを削除

--- getting-started/src/main/java/repository/entity/TodoDetail.java
+++ getting-started/src/main/java/repository/entity/TodoDetail.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -20,7 +18,7 @@ public class TodoDetail {
     private String taskName;
     private String status;
 
-    @ManyToOne
-    @JoinColumn(name = "userId", insertable=false, updatable=false)
-    private Todo todo;
+    // @ManyToOne
+    // @JoinColumn(name = "userId", insertable=false, updatable=false)
+    // private Todo todo;
 }
