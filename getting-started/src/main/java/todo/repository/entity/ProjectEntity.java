017ff4e695e8314cd7c2e214fbc5c80cf1306953
dy <56531152+dy7ss@users.noreply.github.com>
2024-11-25 13:31:19 +0900

    fix: Entityに@EqualsAndHashCodeを追加

--- getting-started/src/main/java/todo/repository/entity/ProjectEntity.java
+++ getting-started/src/main/java/todo/repository/entity/ProjectEntity.java
@@ -13,11 +13,13 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "todo")
 @Data
+@EqualsAndHashCode(callSuper=true)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- getting-started/src/main/java/todo/repository/entity/TaskEntity.java
+++ getting-started/src/main/java/todo/repository/entity/TaskEntity.java
@@ -12,12 +12,14 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "tododetail")
 @Data
+@EqualsAndHashCode(callSuper=true)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
