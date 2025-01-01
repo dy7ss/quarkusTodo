5a63989887ab1e0cc3efccc60e59218c79f0b22f
dy <56531152+dy7ss@users.noreply.github.com>
2025-01-02 04:40:11 +0900

    fix: 不要なimport文の削除

--- getting-started/src/main/java/project/repository/entity/ProjectEntity.java
+++ getting-started/src/main/java/project/repository/entity/ProjectEntity.java
@@ -1,16 +1,11 @@
 package project.repository.entity;
 
 
-import java.util.List;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinColumns;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
